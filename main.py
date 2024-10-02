f1a064be332209ec3826f85d6cd383d9283bb026
DenGrif <grifon991@gmail.com>
2024-10-02 21:32:03 +0400

    Update main01.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 class User:
+
     def __init__(self, user_id, name):
         self.__user_id = user_id
         self.__name = name
-        self.__access_level = 'user' # уровень доступа для обычных пользователей
+        self.__access_level = 'user'  # Уровень доступа для обычных пользователей
 
     # Создание ID пользователя
     def get_user_id(self):
@@ -20,27 +21,30 @@ class User:
     def get_access_level(self):
         return self.__access_level
 
+
 class Admin(User):
+
     def __init__(self, user_id, name):
         super().__init__(user_id, name)
-        self.__access_level = 'admin' # уровень доступа для администратора
+        self.__access_level = 'admin'  # Уровень доступа для администратора
 
-    # Добавление пользователя в систему
-    def add_user(self, user_list, user):
+    @staticmethod
+    def add_user(users, user):
         if isinstance(user, User):
-            user_list.append(user)
-            print(f"Пользователь {user.get_name()} добавлен в ситему")
+            users.append(user)
+            print(f"Пользователь {user.get_name()} добавлен в систему")
         else:
-            print("Ошибка: Объект не является экзепляром класса User.")
+            print("Ошибка: Объект не является экземпляром класса User.")
 
-    # Удаление пользователя из системы
-    def remove_user(self, user_list, user_id):
-        for user in user_list:
+    @staticmethod
+    def remove_user(users, user_id):
+        for user in users:
             if user.get_user_id() == user_id:
-                user_list.remove(user)
+                users.remove(user)
                 print(f"Пользователь {user.get_name()} удалён из системы.")
                 return
         print(f"Пользователь с указанным ID не найден.")
+
 
 # Список для хранения пользователей
 user_list = []
@@ -48,7 +52,7 @@ user_list = []
 # Создаем админа
 admin = Admin(1, "Admin Den")
 
-# Создаем обычного пользователя
+# Создаем обычных пользователей
 user1 = User(234, "Женя")
 user2 = User(235, "Женя5")
 user3 = User(236, "Женя6")
